1d84215c6347340b63318016d88fd336baddd089
Carinepl <caricalima1@gmail.com>
2024-12-03 11:07:26 -0300

    ajustes para navegação entre páginas

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,21 @@ import { TaskDetailsPage } from './pages/TaskDetailsPage'
 import { CreateTaskPage } from './pages/CreateTaskPage'
 import { SearchTaskPage } from './pages/SearchTaskPage'
 import { Page } from './components/layout/navbar/page'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Page />} >
+      <Route path="/" element={<Page/>}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/tasks" element={TaskListPage />} />
+        <Route path="/tasks" element={<TaskListPage />} />
         <Route path="/tasks/:id" element={<TaskDetailsPage />} />
-          <Route path="/create" element={<CreateTaskPage />} />
-          <Route path='/search' element={<SearchTaskPage />} />
-          </Route>
+        <Route path="/create" element={<CreateTaskPage />} />
+        <Route path='/search' element={<SearchTaskPage />} />
+      </Route>
       </Routes>
-      </Router>
-    
+    </Router>
   );
 }
- 
