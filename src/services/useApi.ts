e548ea40c4e2a95984b2967db28a659c5c66db8a
Carinepl <caricalima1@gmail.com>
2024-12-04 11:54:32 -0300

    chore: Alterado estrutura do retorno das tarefas buscadas.

--- src/services/useApi.ts
+++ src/services/useApi.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
-const host = "http://192.168.60.44";
-const port = "5000";
+const host = "http://192.168.60.90";
+const port = 5000;
 
 const baseURL = `${host}:${port}`;
+
+console.log('BASE URL DA CARINE ', baseURL)
 
 const api = axios.create({
     baseURL,
 });
+
 interface CreateTaskProps {
     summary: string;
     description: string;
@@ -14,10 +17,12 @@ interface CreateTaskProps {
     status?: string;
     assignee?: string;
 }
+
 export const useApi = () => {
     return {
         getAllTasks: async () => {
-            return await api.get("/v1/tasks");
+            const result = await api.get("/v1/tasks");
+            return result;
         },
         createTask: async (data: CreateTaskProps) => {
             return await api.post("/v1/tasks", data);
