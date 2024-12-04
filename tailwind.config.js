9a63dc7f04d4b14ab6b902172485092e818243ff
Carinepl <caricalima1@gmail.com>
2024-12-04 11:54:12 -0300

    chore: Configurada dimensões da aplicação com objetivo de definição de propriedades de responsividade.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs:"320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      extend: {},
+      plugins: [],
+    },
   },
-  plugins: [],
-}
-
+};
