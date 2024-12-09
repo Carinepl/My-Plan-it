88dbdc8f1b5731d92c88f3f16535b7aacc35869c
Carinepl <caricalima1@gmail.com>
2024-12-09 11:24:33 -0300

    estilizar input

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,7 +2,7 @@ export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ .
     return (
         <input
             {...rest}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-700 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
         />
     )
 }
