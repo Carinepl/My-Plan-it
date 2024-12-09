9c6f5977e5407690428fe74897458e2e2f95adee
Carinepl <caricalima1@gmail.com>
2024-12-09 11:27:54 -0300

    alterando a estilização

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -3,22 +3,22 @@ import { TaskModel, TaskStatus } from "../../model/TaskModel"
 
 export const Task: React.FC<{ task: TaskModel }> = ({ task }) => {
     return (
-        <div className="bg-white p-4 rounded shadow">
+        <div className="bg-white p-3 rounded shadow">
             <h2 className="text-xl font-semibold">{task.getSummary()}</h2>
             <p className="text-gray-600">{task.getDescription()}</p>
             <div className="flex justify-between items-center mt-2">
                 <span
                     className={
                         `px-2 py-1 rounded
-                                    ${task.getStatus() === TaskStatus.OPEN ? "bg-blue-100 text-blue-800" : ""}
+                                    ${task.getStatus() === TaskStatus.OPEN ? "bg-gray-100 text-black" : ""}
                                     ${task.getStatus() === TaskStatus.IN_PROGRESS ? "bg-yellow-100 text-yellow-800" : ""}
                                     ${task.getStatus() === TaskStatus.UNDER_REVIEW ? "bg-purple-100 text-purple-800" : ""}
-                                    ${task.getStatus() === TaskStatus.DONE ? "bg-green-100 text-green-800" : ""}
+                                    ${task.getStatus() === TaskStatus.DONE ? "bg-green-50 text-green-800" : ""}
                                     `
                     }>
                     {task.getStatus()}
                 </span>
-                <Link to={`/tasks/${task.getId()}`} className="text-blue-600">View Details</Link>
+                <Link to={`/tasks/${task.getId()}`} className="text-gray-500">View Details</Link>
             </div>
         </div>
     )
