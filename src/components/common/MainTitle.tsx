99822e576cfa2bb18b447f4a002a7ba74e665126
Carinepl <caricalima1@gmail.com>
2024-12-09 11:25:39 -0300

    estilizar  titulo principal

--- src/components/common/MainTitle.tsx
+++ src/components/common/MainTitle.tsx
@@ -1,6 +1,6 @@
 export const MainTitle: React.FC<{ title: string }> = ({ title }) => {
     return (
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-purple-950">
             {title}
         </h1>
     )
