b3b3de5691bdc615e8ae5476954e79f21d0e9c0d
Carinepl <caricalima1@gmail.com>
2024-12-09 11:26:19 -0300

    mapear  incones

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom"
 import { NavLink } from "./NavLink"
-import { IconHome, IconList, IconMenu, IconPlus, IconSearch, IconX } from "@tabler/icons-react"
+import { IconHomeFilled, IconChecklist, IconFilePlus, IconSearch, IconX, IconWriting
+} from "@tabler/icons-react"
 import { useState } from "react"
 
 export const Navbar: React.FC = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false)
 
     const handleToggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen)
+
+    const navLinks = [
+        { to: "/", icon: <IconHomeFilled size={18} />, text: "Home" },
+        { to: "/tasks", icon: <IconChecklist size={18} />, text: "Tasks" },
+        { to: "/create", icon: <IconFilePlus size={18} />, text: "Create Tasks" },
+        { to: "/search", icon: <IconSearch size={18} />, text: "Search" }
+    ]
+
     return (
         <nav className="bg-white shadow-md">
             <div className="container mx-auto px-4">
                 <div className="flex justify-between items-center py-4">
-                    <Link to="/" className="text-2xl font-bold text-blue-600">PlanIt</Link>
+                    <Link to="/" className="text-2xl font-bold text-purple-950 flex items-center">
+                        PlanIt
+                        <IconWriting
+                            className="ml-3 text-black" />
+                    </Link>
                     <div
                         className={`
                             space-x-4
@@ -19,37 +32,26 @@ export const Navbar: React.FC = () => {
                             lg:flex 
                     `}
                     >
-                        <NavLink
-                            to="/"
-                            icon={<IconHome size={18} />}
-                            text="Home"
-                        />
-                        <NavLink
-                            to="/tasks"
-                            icon={<IconList size={18} />}
-                            text="Tasks"
-                        />
-                        <NavLink
-                            to="/create"
-                            icon={<IconPlus size={18} />}
-                            text="Create Task"
-                        />
-                        <NavLink
-                            to="/search"
-                            icon={<IconSearch size={18} />}
-                            text="Search"
-                        />
-
+                        {
+                            navLinks.map((link, i) => {
+                                return (
+                                    <NavLink
+                                        key={`key-${i}`}
+                                        to={link.to}
+                                        icon={link.icon}
+                                        text={link.text} />
+                                )
+                            })
+                        }
                     </div>
 
                     <button
                         onClick={handleToggleMobileMenu}
                         className="md:hidden text-gray-500"
                     >
-                        {!isMobileMenuOpen ? <IconMenu size={24} /> : <IconX size={24} />}
+                        {!isMobileMenuOpen ? <IconFilePlus size={24} /> : <IconX size={24} />}
                     </button>
                 </div>
-
                 {
                     isMobileMenuOpen && (
                         <div
@@ -58,26 +60,17 @@ export const Navbar: React.FC = () => {
                             <div
                                 className="space-y-2"
                             >
-                                <NavLink
-                                    to="/"
-                                    icon={<IconHome size={18} />}
-                                    text="Home"
-                                />
-                                <NavLink
-                                    to="/tasks"
-                                    icon={<IconList size={18} />}
-                                    text="Tasks"
-                                />
-                                <NavLink
-                                    to="/create"
-                                    icon={<IconPlus size={18} />}
-                                    text="Create Task"
-                                />
-                                <NavLink
-                                    to="/search"
-                                    icon={<IconSearch size={18} />}
-                                    text="Search"
-                                />
+                                {
+                                    navLinks.map((link) => {
+                                        return (
+                                            <NavLink
+                                                to={link.to}
+                                                icon={link.icon}
+                                                text={link.text} />
+                                        )
+                                    })
+
+                                }
                             </div>
                         </div>
                     )
