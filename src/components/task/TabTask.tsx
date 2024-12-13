7f338e13b8336f54fc73e13dbf6118c50d902d0f
Carinepl <caricalima1@gmail.com>
2024-12-13 11:37:19 -0300

    mudança na estilização

--- src/components/task/TabTask.tsx
+++ src/components/task/TabTask.tsx
@@ -9,6 +9,7 @@ interface TabTaskProps {
 
 export const TabTask: React.FC<TabTaskProps> = ({ onClick, value }) => {
 
+    // TODO: CRIAR PASTA DE CONSTANTES QUE EXPORTAM BUTTONOPTIONS
     const buttonOptions = [
         { type: "ALL", label: "All" },
         { type: TypeProps.EPIC, label: "Epic" },
@@ -19,21 +20,24 @@ export const TabTask: React.FC<TabTaskProps> = ({ onClick, value }) => {
 
     const buttonStyles = (active: boolean) =>
         `px-4 py-2 rounded transition ${active
-            ? "bg-purple-600 text-white shadow-md"
-            : "bg-white text-gray-800 hover:bg-purple-100"
+            ? "bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-500 text-white font-semibold py-2 px-4 rounded shadow-lg transition duration-300"
+            : "bg-white text-gray-800 hover:bg-blue-600"
         }`;
 
     return (
         <div className="flex space-x-4">
-            {buttonOptions.map((button, i) => (
+            {buttonOptions.map((button )=> (
                 <button
-                    key={`k${i}`}
+                    key={button.type}
                     className={buttonStyles(value === button.type)}
                     onClick={() => onClick(button.type as onClickType)}
                 >
                     {button.label}
                 </button>
             ))}
+
         </div>
     );
 };
+
+
