eca6213cb901e412e3567b7e592276e6cf559bf5
Carinepl <caricalima1@gmail.com>
2024-12-13 11:38:41 -0300

    estilizando

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,38 @@
 import { Link } from "react-router-dom"
-import { Button } from "../components/common/Button"
 import { MainTitle } from "../components/common/MainTitle"
 import { useTaskContext } from "../context/useTaskContext"
 import { TaskModel, TaskStatus } from "../model/TaskModel"
+import { useNavigate } from "react-router-dom"
 
 export const HomePage: React.FC = () => {
     const { tasks } = useTaskContext()
+    const navigate = useNavigate();
 
-    const completedTasks = tasks.filter((task:TaskModel) => task.getStatus() === TaskStatus.DONE)
-    
+    const completedTasks = tasks.filter((task: TaskModel) => task.getStatus() === TaskStatus.DONE)
+
     return (
         <div className="space-y-6">
             <MainTitle title="Welcome to PlanIt" />
             <p className="text-xl text-gray-700">Your personal task management system</p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 shadow-md rounded">
-                <StartCard title="Total Tasks" value={tasks.length} />
-                <StartCard title="Completed Tasks" value={completedTasks.length} />
+                <StartCard
+                    title="Total Tasks"
+                    value={tasks.length}
+                    onClick={() => navigate("/tasks")}
+                />
+                <StartCard
+                    title="Completed Tasks"
+                    value={completedTasks.length}
+                    onClick={() => navigate("/completed-tasks")}
+                />
             </div>
 
             <div className="mt-6">
                 <Link to="/create">
-                    <Button variant="primary">
+                    <button className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-500 text-white font-semibold py-2 px-4 rounded shadow-lg transition duration-300">
                         Create Task
-                    </Button>
+                    </button>
+
                 </Link>
             </div>
 
@@ -34,10 +44,10 @@ interface StartCardProps {
     title: string
     value: number
 }
-const StartCard: React.FC<StartCardProps> = ({ title, value }) => {
+const StartCard: React.FC<StartCardProps & { onClick?: () => void }> = ({ title, value, onClick }) => {
     return (
-        <div className="bg-gray-100 p-6 rounded shadow">
-            <h2 className="text-lg font-semi-bold text-gray-500">{title}</h2>
+        <div onClick={onClick} className="bg-gray-100 p-6 rounded shadow cursor-pointer">
+            <h2 className="text-lg font-semi-bold text-gray-600">{title}</h2>
             <p className="text-3xl font-bold text-black">{value}</p>
         </div>
     )
