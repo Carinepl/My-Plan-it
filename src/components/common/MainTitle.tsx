3fb3650332aee2570eaff4642d5b4a7ed5d195e2
Carinepl <caricalima1@gmail.com>
2024-12-13 11:35:41 -0300

    alterações de cores

--- src/components/common/MainTitle.tsx
+++ src/components/common/MainTitle.tsx
@@ -1,6 +1,6 @@
 export const MainTitle: React.FC<{ title: string }> = ({ title }) => {
     return (
-        <h1 className="text-3xl font-bold text-purple-950">
+        <h1 className="text-3xl font-bold text-blue-800">
             {title}
         </h1>
     )
