1642fdfd4b9a791f2255b77873893e33a935f994
Carinepl <caricalima1@gmail.com>
2024-12-13 11:34:45 -0300

    mudança de cor dos botões

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -17,8 +17,8 @@ export const Button: React.FC<ButtonProps> = ({ variant, ...rest }) => {
         focus:outline-none
         focus:rind-2
         focus:ring-offset-2
-        ${variant === "primary" && "border-transparent text-white hover:bg-purple- focus:ring-purple-800 bg-purple-800"}
-        ${variant === "secondary" && "px-4 py-2 border border-gray-300  text-gray-700 hover:bg-gray-50  focus:ring-purple-950"}
+        ${variant === "primary" && "border-transparent text-white hover:bg-purple- focus:ring-blue-700 bg-blue-700"}
+        ${variant === "secondary" && "px-4 py-2 border border-gray-300  text-gray-700 hover:bg-gray-50  focus:ring-blue-700"}
         ${variant === "danger" && "px-4 py-2 border border-red-600  text-white hover:bg-red-700  focus:ring-red-500 bg-red-600"}
        `} >
             {rest.children}
