9a023090f6beb0616c26955fd9622aa0003ce99e
Carinepl <caricalima1@gmail.com>
2024-12-13 11:37:01 -0300

    inclusão do Toaster soner

--- src/components/layout/navbar/page.tsx
+++ src/components/layout/navbar/page.tsx
@@ -1,25 +1,27 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { TaskProvider } from "../../../context/useTaskContext";
-
+import { Toaster } from "sonner";
 
 export const Page: React.FC = () => {
     return (
         <div
-            className="min-h-screen bg-gray-200 text-purple-900 flex flex-col"
+            className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-300 flex flex-col"
         >
             <Navbar />
             
             <main className="flex-grow container mx-auto px-4 py-8">
                 <TaskProvider>
                     <Outlet />
+                    <Toaster richColors />
                 </TaskProvider>
             </main>
-            <footer className="bg-white shadow-md mt-8">
+            <footer className="bg-white shadow-md mt-7">
                 <div className="container mx-auto p-4 text-center text-purple">
                     &copy; 2024 PlanIt. All rights reserved.
                 </div>
             </footer>
         </div>
     )
+    
 }
