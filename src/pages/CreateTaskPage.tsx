51d06fd6652b2c8102047f579ce289f70908db1c
Carinepl <caricalima1@gmail.com>
2024-12-13 11:38:07 -0300

    importando soner

--- src/pages/CreateTaskPage.tsx
+++ src/pages/CreateTaskPage.tsx
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom"
 import { MainTitle } from "../components/common/MainTitle"
 import { Label } from "../components/common/Label"
 import { Input } from "../components/common/Input"
-import { Button } from "../components/common/Button"
 import { TypeProps } from "../model/TaskModel"
 import { useApi } from "../services/useApi"
+import { toast } from "sonner"
 
 interface TaskFormDataProps {
     summary: string
@@ -25,6 +25,37 @@ export const CreateTaskPage: React.FC = () => {
     })
     const api = useApi()
 
+
+    const useReporters = [
+        "Alan Maxwell",
+        "Lucas Granjense",
+        "Wesley Ximenes",
+        "Carine Lima",
+        "Flavio Montoril",
+        "Maria Rita",
+        "Mathues Monteiro"
+    ];
+
+    const useAssignee = [
+        {value:"Alan Maxwell"},
+        {value:"Lucas Granjense"},
+        {value:"Wesley Ximenes"},
+        {value:"Carine Lima"},
+        {value:"Flavio Montoril"},
+        {value:"Maria Rita"},
+        {value:"Mathues Monteiro"}
+    ];
+
+    const typesTasks = [
+        { value: "TASK" },
+        { value: "BUG" },
+        { value: "EPIC" },
+        { value: "SUB_TASK" }
+    ]
+
+
+
+
     const handleInputTaskChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
 
         const { name, value } = e.target
@@ -38,21 +69,31 @@ export const CreateTaskPage: React.FC = () => {
         e.preventDefault()
         try {
             const response = await api.createTask(formData)
+            toast.success('Task created successfully!', {
+                style: {
+                    backgroundColor: '#3498db',
+                    color: '#fff',
+                    borderRadius: '8px',
+                    boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+                },
+            });
+
             if (response.status === 201) {
                 navigate('/tasks')
             } else {
                 window.alert('Error to create task')
             }
         } catch (error) {
-            console.log(error)
+            toast.error('Failed to create tarsk!');
         }
+
 
     }
 
     return (
         <div className="space-y-6">
             <MainTitle title="Create New Task" />
-            <form onSubmit={handleSubmit} className="space-y-4 bg-white p-6 rounded shadow">
+            <form onSubmit={handleSubmit} className="space-y-4 bg-blue-50 p-7 rounded shadow">
                 <div>
                     <Label text="Summary" htmlFor="summary" />
                     <Input
@@ -81,67 +122,48 @@ export const CreateTaskPage: React.FC = () => {
 
                 <div>
                     <Label text="Type" htmlFor="type" />
-                    <select
-                        id="type"
+                    <select id="type"
                         name="type"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                        onChange={handleInputTaskChange}
-                        value={formData.type}
-                        required
-                    >
-                        <option value="TASK">Task</option>
-                        <option value="BUG">Bug</option>
-                        <option value="EPIC">Epic</option>
-                        <option value="SUB_TASK">Subtask</option>
+                        className="...">
+                            {/* MEU CÓDIGO */}
+                        {typesTasks.map(type => (
+                            <option key={type.value} value={type.value}>
+                                {type.value}
+                            </option>
+                        ))}
+
+
                     </select>
                 </div>
 
 
                 <div>
                     <Label text="Reporter" htmlFor="reporter" />
-                    <select
-                        id="reporter"
-                        name="reporter"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                        onChange={handleInputTaskChange}
-                        required
-                        value={formData.reporter}
-                    >
-                        <option value="Alan Maxwell">Alan Maxwell</option>
-                        <option value="Lucas Granjense">Lucas Granjense</option>
-                        <option value="Wesley Ximenes">Wesley Ximenes</option>
-                        <option value="Carine Lima">Carine Lima</option>
-                        <option value="Flavio Montoril">Flavio Montoril</option>
-                        <option value="Maria Rita">Maria Rita</option>
-                        <option value="Mathues Monteiro">Mathues Monteiro</option>
-
+                    {/* MEU CÓDIGO */}
+                    <select id="reporter" name="reporter" className="...">
+                        {useReporters.map(reporter => (
+                            <option key={reporter} value={reporter}>{reporter}</option>
+                        ))}
                     </select>
+
                 </div>
 
                 <div>
                     <Label text="Assignee" htmlFor="assignee" />
-                    <select
-                        id="assignee"
-                        name="assignee"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                        onChange={handleInputTaskChange}
-                        value={formData.assignee}
-                    >
-                        <option value=""></option>
-                        <option value="Alan Maxwell">Alan Maxwell</option>
-                        <option value="Lucas Granjense">Lucas Granjense</option>
-                        <option value="Wesley Ximenes">Wesley Ximenes</option>
-                        <option value="Carine Lima">Carine Lima</option>
-                        <option value="Flavio Montoril">Flavio Montoril</option>
-                        <option value="Maria Rita">Maria Rita</option>
-                        <option value="Mathues Monteiro">Mathues Monteiro</option>
-
+                    {/* MEU CÓDIGO */}
+                    <select id="assignee" name="assignee" className="...">
+                        {useAssignee.map(assignee => (
+                            <option key={assignee.value} value={assignee.value}>{assignee.value}</option>
+                        ))}
                     </select>
+
                 </div>
+
                 <div className="flex justify-end">
-                    <Button type="submit" variant="primary">
+                    <button className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700
+                     hover:to-blue-500 text-white font-semibold py-2 px-4 rounded shadow-lg transition duration-300">
                         Create Task
-                    </Button>
+                    </button>
                 </div>
 
             </form>
