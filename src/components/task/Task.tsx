601c3bb8bf36f62e94d486e03f738f9d77bfae27
Carinepl <caricalima1@gmail.com>
2024-12-13 11:37:41 -0300

    mudança de cor

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -7,12 +7,13 @@ export const Task: React.FC<{ task: TaskModel }> = ({ task }) => {
             <h2 className="text-xl font-semibold">{task.getSummary()}</h2>
             <p className="text-gray-600">{task.getDescription()}</p>
             <div className="flex justify-between items-center mt-2">
+            
                 <span
                     className={
                         `px-2 py-1 rounded
                                     ${task.getStatus() === TaskStatus.OPEN ? "bg-gray-100 text-black" : ""}
                                     ${task.getStatus() === TaskStatus.IN_PROGRESS ? "bg-yellow-100 text-yellow-800" : ""}
-                                    ${task.getStatus() === TaskStatus.UNDER_REVIEW ? "bg-purple-100 text-purple-800" : ""}
+                                    ${task.getStatus() === TaskStatus.UNDER_REVIEW ? "bg-purple-100 text-blue-600" : ""}
                                     ${task.getStatus() === TaskStatus.DONE ? "bg-green-50 text-green-800" : ""}
                                     `
                     }>
